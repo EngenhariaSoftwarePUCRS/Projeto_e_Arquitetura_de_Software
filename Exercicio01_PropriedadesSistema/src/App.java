8a64a60b72e85fdab132d7d48597dfc9a6974ba5
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2024-02-27 00:27:01 +0000

    System Properties

--- Exercicio01_PropriedadesSistema/src/App.java
+++ Exercicio01_PropriedadesSistema/src/App.java
@@ -1,7 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("\nHi!");
-        String version = System.getProperty("java.version");
-        System.out.printf("Running Java Version %s\n", version);
+        String[] systemProperties = {
+            "Java Version", "Java Class Path", "Java Home", "Java Vendor", "Java Vendor URL",
+            "File Separator", "Line Separator", "Path Separator",
+            "OS Arch", "OS Name", "OS Version",
+            "User Dir", "User Home", "User Name"
+        };
+        for (String property : systemProperties) {
+            String systemProperty = property.toLowerCase().replace(" ", ".");
+            String output = System.getProperty(systemProperty);
+            System.out.printf("%s: %s\n", property, output);
+        }
     }
 }
