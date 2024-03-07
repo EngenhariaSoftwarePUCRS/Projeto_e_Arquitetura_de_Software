f9cbcacf1a08aad7a86b74e7e3455716a107805f
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2024-03-07 00:12:21 +0000

    Aula04 - Exercicio Motivador - V3.1: SUV

--- Aula04__PadroesDeProjeto/ExercicioMotivadorPadroes/src/App.java
+++ Aula04__PadroesDeProjeto/ExercicioMotivadorPadroes/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
-        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
+        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, TipoCombustivel.DIESEL, 5, 70);
+        Carro suv = new Carro("SUV", TipoCombustivel.GASOLINA, TipoCombustivel.FLEX, 8, 55);
 
         Carro[] cars = {basico, esportivo, utilitario, suv};
         printCars(cars);
--- Aula04__PadroesDeProjeto/ExercicioMotivadorPadroes/src/Carro.java
+++ Aula04__PadroesDeProjeto/ExercicioMotivadorPadroes/src/Carro.java
@@ -4,10 +4,15 @@ public class Carro {
     private Motor motor;
     private TanqueCombustivel tanque;
 
-    public Carro(String modelo, TipoCombustivel tipoCombustivel, int consumoMotor, int capacidadeTanque) {
+    public Carro(
+        String modelo,
+        TipoCombustivel tipoCombustivelMotor,
+        TipoCombustivel tipoCombustivelTanque,
+        int consumoMotor,
+        int capacidadeTanque) {
         this.modelo = modelo;
-        motor = new Motor(tipoCombustivel, consumoMotor);
-        tanque = new TanqueCombustivel(tipoCombustivel, capacidadeTanque);
+        motor = new Motor(tipoCombustivelMotor, consumoMotor);
+        tanque = new TanqueCombustivel(tipoCombustivelTanque, capacidadeTanque);
     }
 
     public String getModelo() {
