8de6e2f29d32ef262facaa1b6f67a661b00001b4
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-01 20:12:23 -0300

    Arquitetura Web - Parte 6 - Repository

---
+++ Topico_02__Arquitetura_WEB/Parte_06__Padrao_Repository/src/test/java/com/parte_06__padrao_repository/ex1biblioeca/Ex1BiblioecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parte_06__padrao_repository.ex1biblioeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1BiblioecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
