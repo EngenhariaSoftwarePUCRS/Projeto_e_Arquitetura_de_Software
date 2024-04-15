fafa66ed3f8c5ce4a9e5eb5f71a9dd6eb5191eb5
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-15 20:11:19 -0300

    Sistemas Monoliticos - Parte 3 - Sistema Vendas DDD

---
+++ Topico_03__Sistemas_Monoliticos/Parte_03__Projeto_Baseado_em_Dominio/src/test/java/com/projarq/SistVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projarq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
