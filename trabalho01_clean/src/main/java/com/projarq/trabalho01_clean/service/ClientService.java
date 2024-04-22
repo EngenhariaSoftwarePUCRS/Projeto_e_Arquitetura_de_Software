93cbd115d333c970f7be520fa32ef3d940b41e68
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-22 20:34:36 -0300

    Service Skeleton: Client
    Co-Authored-By: Luiza Heller Kroeff Plá <103202754+LuHellerKP@users.noreply.github.com>
    Co-Authored-By: Carolina Ferreira <151643887+carolmicfer@users.noreply.github.com>
    Co-Authored-By: Mateus Caçabuena <mateuscacabuena@gmail.com>

--- trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/ClientService.java
+++ trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/ClientService.java
@@ -1,5 +1,35 @@
 package com.projarq.trabalho01_clean.service;
 
-public class ClientService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.projarq.trabalho01_clean.entity.ClientEntity;
+import com.projarq.trabalho01_clean.repository.IClientRepository;
+
+public class ClientService implements IClientRepository {
+    private JdbcTemplate database;
+
+    @Autowired
+    public ClientService(JdbcTemplate database) {
+        this.database = database;
+    }
+
+    @Override
+    public ClientEntity create(Long id, String name, String email) {
+        return null;
+    }
+
+    @Override
+    public ClientEntity edit(Long id, String name, String email) {
+        return null;
+    }
+
+    @Override
+    public List<ClientEntity> getAllClients() {
+        return null;
+    }
+
     
 }
