5e824944abe34a4a17a2546218c732dc7fcb47be
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-04-21 22:45:17 -0300

    fix: SQL Scripts and Table Names
    Co-Authored-By: Mateus Caçabuena <mateuscacabuena@gmail.com>
    Co-Authored-By: Carolina Ferreira <151643887+carolmicfer@users.noreply.github.com>
    Co-Authored-By: Luiza Heller Kroeff Plá <103202754+luhellerkp@users.noreply.github.com>

--- trabalho01_clean/src/main/resources/create.sql
+++ trabalho01_clean/src/main/resources/create.sql
@@ -1,14 +1,14 @@
-DROP TABLE app IF EXISTS;
-CREATE TABLE app (id BIGINT, name VARCHAR(255), monthlyCost DECIMAL(4,2), PRIMARY KEY(id));
+DROP TABLE apps IF EXISTS;
+CREATE TABLE apps (id BIGINT, name VARCHAR(255), monthlyCost DECIMAL(4,2), PRIMARY KEY(id));
 
-DROP TABLE client IF EXISTS;
-CREATE TABLE client (id BIGINT, name VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id));
+DROP TABLE clients IF EXISTS;
+CREATE TABLE clients (id BIGINT, name VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id));
 
-DROP TABLE signature IF EXISTS;
-CREATE TABLE signature (id BIGINT, appId BIGINT, clientId BIGINT, startDate DATE, endDate DATE, PRIMARY KEY(id), FOREIGN KEY(appId) REFERENCES app(id), FOREIGN KEY(clientId) REFERENCES client(id));
+DROP TABLE signatures IF EXISTS;
+CREATE TABLE signatures (id BIGINT, appId BIGINT, clientId BIGINT, startDate DATE, endDate DATE, PRIMARY KEY(id), FOREIGN KEY(appId) REFERENCES apps(id), FOREIGN KEY(clientId) REFERENCES clients(id));
 
-DROP TABLE payment IF EXISTS;
-CREATE TABLE payment (id BIGINT, signatureId BIGINT, payedValue DECIMAL(4,2), paymentDate DATE, promotion VARCHAR(100), PRIMARY KEY(id), FOREIGN KEY(signatureId) REFERENCES signature(id));
+DROP TABLE payments IF EXISTS;
+CREATE TABLE payments (id BIGINT, signatureId BIGINT, payedValue DECIMAL(4,2), paymentDate DATE, promotion VARCHAR(100), PRIMARY KEY(id), FOREIGN KEY(signatureId) REFERENCES signatures(id));
 
-DROP TABLE user IF EXISTS;
-CREATE TABLE user (id BIGINT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id));
+DROP TABLE users IF EXISTS;
+CREATE TABLE users (id BIGINT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id));
