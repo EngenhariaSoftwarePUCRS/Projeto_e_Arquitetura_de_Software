3babb3693a4fbdf344d341da90bbcc4720f650cc
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-22 20:36:40 -0300

    Service Skeleton: Payment
    Co-Authored-By: Luiza Heller Kroeff Plá <103202754+LuHellerKP@users.noreply.github.com>
    Co-Authored-By: Carolina Ferreira <151643887+carolmicfer@users.noreply.github.com>
    Co-Authored-By: Mateus Caçabuena <mateuscacabuena@gmail.com>

--- trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/PaymentService.java
+++ trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/PaymentService.java
@@ -1,5 +1,36 @@
 package com.projarq.trabalho01_clean.service;
 
-public class PaymentService {
-    
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.projarq.trabalho01_clean.entity.PaymentEntity;
+import com.projarq.trabalho01_clean.repository.IPaymentRepository;
+
+@Repository
+public class PaymentService implements IPaymentRepository {
+    private JdbcTemplate database;
+
+    @Autowired
+    public PaymentService(JdbcTemplate database) {
+        this.database = database;
+    }
+
+    @Override
+    public PaymentEntity create(Long signatureId, float payedValue, Date paymentDate, String promotion) {
+        return null;
+    }
+
+    @Override
+    public PaymentEntity edit(Long id, String name, float monthlyCost) {
+        return null;
+    }
+
+    @Override
+    public List<PaymentEntity> getAll() {
+        return null;
+    }
 }
