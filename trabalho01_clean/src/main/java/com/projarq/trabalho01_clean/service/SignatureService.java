67a68c0109c8584cb1d10cd13c867c3b85eaf7a1
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-22 20:36:45 -0300

    Service Skeleton: Signature
    Co-Authored-By: Luiza Heller Kroeff Plá <103202754+LuHellerKP@users.noreply.github.com>
    Co-Authored-By: Carolina Ferreira <151643887+carolmicfer@users.noreply.github.com>
    Co-Authored-By: Mateus Caçabuena <mateuscacabuena@gmail.com>

--- trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/SignatureService.java
+++ trabalho01_clean/src/main/java/com/projarq/trabalho01_clean/service/SignatureService.java
@@ -1,5 +1,46 @@
 package com.projarq.trabalho01_clean.service;
 
-public class SignatureService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.projarq.trabalho01_clean.entity.SignatureEntity;
+import com.projarq.trabalho01_clean.repository.ISignatureRepository;
+
+@Repository
+public class SignatureService implements ISignatureRepository {
+    private JdbcTemplate database;
+
+    @Autowired
+    public SignatureService(JdbcTemplate database) {
+        this.database = database;
+    }
+
+    @Override
+    public SignatureEntity addSignature(Long clientId, Long appId) {
+        return null;
+    }
+
+    @Override
+    public List<SignatureEntity> getAppSignatures(Long appId) {
+        return null;
+    }
+
+    @Override
+    public List<SignatureEntity> getClientSignatures(Long clientID) {
+        return null;
+    }
+
+    @Override
+    public List<SignatureEntity> getSignatureByType(Long appId, String type) {
+        return null;
+    }
+
+    @Override
+    public boolean isSignatureActive(Long signatureId) {
+        return false;
+    }
     
 }
