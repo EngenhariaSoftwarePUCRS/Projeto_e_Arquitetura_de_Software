247eadd376e145fcf6a7af00a10fd6224b738822
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-06-04 20:52:15 -0300

    Trabalho 01 - Fix DB Create Script
    Co-Authored-By: Mateus Caçabuena <mateuscacabuena@gmail.com>

--- trabalho01_clean/src/main/resources/create.sql
+++ trabalho01_clean/src/main/resources/create.sql
@@ -1,14 +1,41 @@
-DROP TABLE apps IF EXISTS;
-CREATE TABLE apps (id BIGINT AUTO_INCREMENT, name VARCHAR(255), monthlyCost DECIMAL(4,2), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS apps (
+    id BIGINT AUTO_INCREMENT,
+    name VARCHAR(255),
+    monthlyCost DECIMAL(4,2),
+    PRIMARY KEY(id)
+);
 
-DROP TABLE clients IF EXISTS;
-CREATE TABLE clients (id BIGINT, name VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS clients (
+    id BIGINT AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    PRIMARY KEY(id)
+);
 
-DROP TABLE signatures IF EXISTS;
-CREATE TABLE signatures (id BIGINT, appId BIGINT, clientId BIGINT, startDate DATE, endDate DATE, PRIMARY KEY(id), FOREIGN KEY(appId) REFERENCES apps(id), FOREIGN KEY(clientId) REFERENCES clients(id));
+CREATE TABLE IF NOT EXISTS signatures (
+    id BIGINT AUTO_INCREMENT,
+    appId BIGINT,
+    clientId BIGINT,
+    startDate DATE,
+    endDate DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(appId) REFERENCES apps(id),
+    FOREIGN KEY(clientId) REFERENCES clients(id)
+);
 
-DROP TABLE payments IF EXISTS;
-CREATE TABLE payments (id BIGINT, signatureId BIGINT, payedValue DECIMAL(4,2), paymentDate DATE, promotion VARCHAR(100), PRIMARY KEY(id), FOREIGN KEY(signatureId) REFERENCES signatures(id));
+CREATE TABLE IF NOT EXISTS payments (
+    id BIGINT AUTO_INCREMENT,
+    signatureId BIGINT,
+    payedValue DECIMAL(4,2),
+    paymentDate DATE,
+    promotion VARCHAR(100),
+    PRIMARY KEY(id),
+    FOREIGN KEY(signatureId) REFERENCES signatures(id)
+);
 
-DROP TABLE users IF EXISTS;
-CREATE TABLE users (id BIGINT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT AUTO_INCREMENT,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY(id)
+);
