bcbc3459c8153870d4ccd0d08c2d34e27fea5002
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-06-09 22:10:34 -0300

    Topico 04 - Micro Servi;os - Parte 01 - Introdução

---
+++ Topico_04__Arquitetura_Micro_Servicos/Parte_01__Introducao/currency-exchange-service/src/main/java/com/projarq/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,7 @@
+package com.projarq.currencyexchangeservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyExchangeRepository extends CrudRepository<CurrencyExchange,Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
