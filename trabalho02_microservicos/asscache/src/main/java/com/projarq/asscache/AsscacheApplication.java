d237978e633a42dcbb1dd8926b088095403b01c4
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-06-30 20:40:57 -0300

    T2 | Nameserver & Gateway on Docker Compose

--- trabalho02_microservicos/asscache/src/main/java/com/projarq/asscache/AsscacheApplication.java
+++ trabalho02_microservicos/asscache/src/main/java/com/projarq/asscache/AsscacheApplication.java
@@ -3,11 +3,13 @@ package com.projarq.asscache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaRepositories(basePackages = "com.projarq.asscache.repository")
 @EntityScan(basePackages = "com.projarq.asscache.model")
 public class AsscacheApplication {
--- trabalho02_microservicos/asscache/src/main/java/com/projarq/asscache/Controller/SignatureController.java
+++ trabalho02_microservicos/asscache/src/main/java/com/projarq/asscache/Controller/SignatureController.java
@@ -2,37 +2,33 @@ package com.projarq.asscache.controller;
 
 import com.projarq.asscache.model.Signature;
 import com.projarq.asscache.service.CacheService;
+import com.projarq.asscache.service.SCAAProxy;
 import com.projarq.asscache.service.SignatureService;
 
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SignatureController {
+    private SCAAProxy scaaProxy;
     private SignatureService signatureService;
     private CacheService cacheService;
-    private RestTemplate restTemplate;
-
-    @Value("${scaa.url}")
-    private String scaaUrl;
 
     @Autowired
     public SignatureController(
+        SCAAProxy scaaProxy,
         SignatureService signatureService,
-        CacheService cacheService,
-        RestTemplate restTemplate
+        CacheService cacheService
     ) {
+        this.scaaProxy = scaaProxy;
         this.signatureService = signatureService;
         this.cacheService = cacheService;
-        this.restTemplate = restTemplate;
     }
 
     @GetMapping
@@ -48,19 +44,15 @@ public class SignatureController {
     @GetMapping("/{signatureId}")
     public ResponseEntity<Signature> checkSignature(@PathVariable String signatureId) {
         System.out.println("Checking signature for signatureId: " + signatureId);
-        System.out.println("\n\n\tSCAA URL: " + scaaUrl + "\n\n");
         Signature signature = cacheService.getSignature(signatureId);
         if (signature == null) {
-            String path = scaaUrl + "/assinaturas/validade/" + signatureId;
             try {
-                ResponseEntity<Date> response = restTemplate.getForEntity(path, Date.class);
-                Date expiryDate = response.getBody();
+                Date expiryDate = scaaProxy.getSignatureExpiryDate(signatureId);
                 signature = new Signature(
                     Long.parseLong(signatureId),
                     expiryDate.after(new Date()),
                     expiryDate
                 );
-                System.out.println("Response: " + response);
             } catch (HttpClientErrorException e) {
                 System.out.println("Error: " + e.getMessage());
                 return ResponseEntity.notFound().build();
