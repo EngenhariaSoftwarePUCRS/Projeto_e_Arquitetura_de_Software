f801c5c0fc6aa1aedb117cab73e437134f4c0dfe
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-06-30 00:18:57 -0300

    T2 - AssCache Folder

---
+++ trabalho02_microservicos/asscache/src/main/java/com/projarq/asscache/AsscacheApplication.java
@@ -0,0 +1,13 @@
+package com.projarq.asscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsscacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsscacheApplication.class, args);
+	}
+
+}
